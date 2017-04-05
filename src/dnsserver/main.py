00b1d94ca351f1a272d9fb6c4a7c76fb896e5486
Timidger <APragmaticPlace@gmail.com>
2017-04-05 11:06:43 -0400

    More changes

--- src/dnsserver/main.py
+++ src/dnsserver/main.py
@@ -17,6 +17,9 @@ DEFAULT_SERVERS=[
     'ec2-54-233-152-60.sa-east-1.compute.amazonaws.com',
 ]
 
+#TODO Remove
+ORIGIN_SERVER = ("ec2-54-166-234-74.compute-1.amazonaws.com", 8080)
+
 class DNSServer:
     def __init__(self, name, port, servers=DEFAULT_SERVERS):
         self.name = name
@@ -35,7 +38,14 @@ class DNSServer:
                 data, address = self.sock.recvfrom(2048)
                 query = DNSQuery(data)
                 print("Got data from {}".format(address))
+                print("Received: \n{}\n".format(data))
+                # TODO Don't give the origin server as a place to go
+                server = socket.getaddrinfo(ORIGIN_SERVER[0], ORIGIN_SERVER[1])[0][-1][0]
+                query.domain = server
+                packet = query.respond(server)
+                print("Sending packet\n\"{}\"\n".format(packet))
                 self.sock.sendto(data, address)
+                print("Packet sent")
                 # TODO Lookup address in hashmap, send them to that one.
 
             except (socket.timeout, socket.error):
@@ -45,7 +55,6 @@ class DNSServer:
                     pass
 
     def close(self):
-        #self.sock.shutdown(socket.SHUT_RDWR)
         self.sock.close()
 
 class DNSQuery:
@@ -53,24 +62,24 @@ class DNSQuery:
     self.data = data
     self.domain = ''
 
-    tipo = (data[2] >> 3) & 15   # Opcode bits
-    if tipo == 0:                     # Standard query
-      ini = 12
-      lon = data[ini]
-      while lon != 0:
-        self.domain += data[ini + 1: ini + lon + 1] + '.'
-        ini += lon + 1
-        lon = data[ini]
+    head = (data[2] >> 3) & 15
+    if head == 0:
+        ini = 12
+        length = data[ini]
+        while length != 0:
+            self.domain += data[ini + 1: ini + length + 1] + '.'
+            ini += length + 1
+            length = data[ini]
 
-  def respuesta(self, ip):
+  def respond(self, ip):
     packet=''
     if self.domain:
-      packet += self.data[:2] + "\x81\x80"
-      packet += self.data[4:6] + self.data[4:6] + '\x00\x00\x00\x00'   # Questions and Answers Counts
-      packet += self.data[12:]                                         # Original Domain Name Question
-      packet += '\xc0\x0c'                                             # Pointer to domain name
-      packet += '\x00\x01\x00\x01\x00\x00\x00\x3c\x00\x04'             # Response type, ttl and resource data length -> 4 bytes
-      packet += str.join('', map(lambda x: chr(int(x)), ip.split('.'))) # 4bytes of IP
+        packet += str(self.data[:2]) + "\x81\x80"
+        packet += str(self.data[4:6]) + str(self.data[4:6]) + '\x00\x00\x00\x00'   # Questions and Answers Counts
+        packet += str(self.data[12:])                                         # Original Domain Name Question
+        packet += '\xc0\x0c'                                             # Pointer to domain name
+        packet += '\x00\x01\x00\x01\x00\x00\x00\x3c\x00\x04'             # Response type, ttl and resource data length -> 4 bytes
+        packet += str.join('', map(lambda x: chr(int(x)), ip.split('.'))) # 4bytes of IP
     return packet
 
 def main():
